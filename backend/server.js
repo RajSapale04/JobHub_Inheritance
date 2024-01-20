7ebd0b4f42136ff5e01e4ea1391d0992edc539b0
Raj <raj4sapale4@gmail.com>
2024-01-20 20:41:13 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.json({mssg:'Welcome to the app'})
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log('listening on port 3000')
+})
