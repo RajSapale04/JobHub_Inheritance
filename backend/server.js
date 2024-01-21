098ad897f3e8a4cac8070cd671a0dab1f41ca7ec
Raj <raj4sapale4@gmail.com>
2024-01-21 13:15:01 +0530

    routes added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const UserRoutes = require('./routes/users')
+const JobRoutes = require('./routes/jobs')
+const CompanyRoutes = require('./routes/companies')
 
 const app = express()
+
+
+
 
 app.get('/',(req,res)=>{
     res.json({mssg:'Welcome to the app'})
