72320d1629e3e4e6a8740341f2991903757c1be4
VedantAswale <156321756+VedantAswale@users.noreply.github.com>
2024-01-22 23:44:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,9 @@ const app = express()
 const mongoose = require('mongoose');
 const db = 'mongodb+srv://Vedant:Vedant@03@jobhub.shufb5z.mongodb.net/database?retryWrites=true&w=majority';
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
-   .then((result) => {
-     app.listen(process.env.PORT,()=>{
-         console.log('listening on port 3000')
-     })
-    .catch(err) => console.log(err); 
-  }
-  
+   .then(() => {
+    app.listen(process.env.PORT, () => console.log('listening on port 3000'));
+   })
+   .catch((err) => console.log(err));  
 
-app.get('/',(req,res)=>{
-    res.json({mssg:'Welcome to the app'})
-})
+app.get('/',(req,res) => res.json({mssg:'Welcome to the app'}))
