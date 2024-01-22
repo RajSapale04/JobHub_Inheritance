43f7e6f5e73eb13ef1e16afca89bfd565e5bdf65
VedantAswale <156321756+VedantAswale@users.noreply.github.com>
2024-01-22 23:39:42 +0530

    schema update

--- schema.js
+++ schema.js
@@ -1,1 +1,16 @@
+const mongoose =  require('mongoose');
+const Schema = mongoose.Schema;
 
+const login = new Schema({
+    email: {
+        type: String,
+        required: true 
+    },
+    password:{
+        type: String,
+        required: true
+    }
+}, { timestamps: true});
+
+const Login = mongoose.model('Information', login);
+module.exports = Login; 
