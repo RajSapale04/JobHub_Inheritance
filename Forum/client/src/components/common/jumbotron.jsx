ab45ad5e8f1643837fef5b5999f7c617366196c2
Mit <mitmayekar10@gmail.com>
2024-01-26 20:51:42 +0530

    commit2

---
+++ Forum/client/src/components/common/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">JobHub Forum</h1>
+      <p class="lead">We &lt;3 people who are eager to!</p>
+      <hr class="my-4" />
+      <p className="lead">A discussion forum for job seekers.</p>
+    </div>
+  );
+};
+
+export default Jumotron;
